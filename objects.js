56fcf161667289f8643fedfef233cdf324ef5554
petercortez00 <33037106+petercortez00@users.noreply.github.com>
2018-03-30 12:01:11 -0700

    Create objects.js

---
+++ objects.js
@@ -0,0 +1,25 @@
+# ObjectsInSchool
+var id = 1;
+function Student(firstName,lastName,grade){
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.grade = grade;
+    this.id = id++;
+}
+
+function Teacher(firstName,lastName,subject){
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.subject = subject;
+    this.id = id++;
+
+
+}
+
+function Section(name,size){
+    this.name = name;
+    this.size = size;
+    this.teachers = [];
+    this.students = [];
+    this.id = id++;
+}
